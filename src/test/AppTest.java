99aded9253a51e04e49638ce16d273902687519e
Devender Musukula <devenderreddy4102@outlook.com>
2023-01-02 11:47:12 +0530

    Updated App Test.java file

--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -1,20 +1,38 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import juni.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
+    extends TestCase
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest( String testName )
+	{
+		super( testName )
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite( AppTest.class )
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp()
+	{
+		assertTrue( true );
+	}	
+}	
